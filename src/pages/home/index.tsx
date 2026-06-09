8b46951afd366fbdec518c86c850df50a3d1905c
yujeong <yujeong.choung@gmail.com>
2026-06-09 15:46:14 +0900

    feat: 홈 활동 요약 섹션 구현 (비로그인)
    - useHomeSummary 훅 추가: summaryGet 호출, isAuthReady 이후 viewer 키로 캐시 분리
    - home/queryKeys.ts에 summary/upcoming 쿼리 키 팩토리 추가
    - ActivitySummary 섹션: publicSummary의 올해 이벤트 수·누적 거리 노출
    - 접근성: section+h2 위계, 수치는 색 아닌 텍스트로 전달, 스크린리더용 문장(HiddenText) 제공, 로딩/에러 상태 처리

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,13 +4,15 @@ import styled from '@emotion/styled';
 
 import { PageLayout, TopNavigation } from '@/components';
 
+import { ActivitySummary } from './components/ActivitySummary';
+
 export const MainPage = (): ReactElement => {
   return (
     <PageLayout background="gradient.bg.brand-main">
       <TopNavigation aria-label="홈 상단 메뉴" title="홈화면" />
       <Content>
         {/* C4: 이벤트 검색 진입 바 */}
-        {/* C2: 활동 요약 섹션 */}
+        <ActivitySummary />
         {/* C3: 다가오는 러닝 모임 섹션 */}
       </Content>
     </PageLayout>
