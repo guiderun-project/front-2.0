4666ab8e6ec5a800d987645bb06ad595be62cd37
Pridesd <whwotjr98@naver.com>
2026-06-28 15:53:50 +0900

    feat: add subtle footer button background

--- src/components/FooterButton/FooterButton.types.ts
+++ src/components/FooterButton/FooterButton.types.ts
@@ -2,7 +2,10 @@ import type { ComponentPropsWithoutRef, ReactNode } from 'react';
 
 import type { ButtonGroupRatio } from '../Button';
 
+export type FooterButtonBackground = 'footer' | 'subtle';
+
 export type FooterButtonProps = {
+  background?: FooterButtonBackground;
   children: ReactNode;
   ratio?: ButtonGroupRatio;
   reserveSpace?: boolean;
